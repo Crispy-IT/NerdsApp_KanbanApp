7512efe333158c0254c2f484ee46dfc1532d642b
Tomasz Skorupski <skorupa7@o2.pl>
2026-04-02 04:00:51 +0200

    fix: use EnsureCreated instead of Migrate

--- KanbanApp.Backend/Program.cs
+++ KanbanApp.Backend/Program.cs
@@ -77,7 +77,7 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     if (db.Database.IsRelational())
     {
-        db.Database.Migrate();
+        db.Database.EnsureCreated();
     }
 }
 
